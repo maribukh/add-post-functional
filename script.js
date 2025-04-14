ad2605dedfcd454fc7e1ce7703bd9de21572fb79
Mariam <mariebukhaidze@gmail.com>
2025-04-14 21:02:11 +0400

    Edit some Functional and Style

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function togglePostDetails() {
   let postContainer = document.querySelector(".post-container");
-
   if (
     postContainer.style.display === "none" ||
     postContainer.style.display === ""
@@ -11,38 +10,64 @@ function togglePostDetails() {
   }
 }
 
-function toggleAddPost() {
-  let;
-}
-
 function deleteButton(element) {
   let postContainer = element.closest(".post-container");
   postContainer.style.display = "none";
-
-  let input = document.querySelector(".input-container input");
-
-  input.value = "";
+  document.querySelector(".input-container input").value = "";
 }
 
 function addPost() {
   let input = document.querySelector(".input-container input");
   let post = document.querySelector(".post");
+  let postBox = document.createElement("div");
+  let personOriginal = document.querySelector(".person");
+  let personClone = personOriginal.cloneNode(true);
   let textPost = document.createElement("p");
   let postContainer = document.querySelector(".post-container");
 
+  let selectInClone = personClone.querySelector("select");
+  if (selectInClone) {
+    selectInClone.remove();
+  }
+
+  let imgBgd = personClone.querySelector(".img-bgd");
+  if (imgBgd) {
+    imgBgd.style.backgroundImage = "url('./images/profile-photo.jpg')";
+  }
+
+  let dateTime = document.createElement("span");
+  let now = new Date();
+  let options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  dateTime.textContent = now.toLocaleString("en-US", options);
+  dateTime.className = "timestamp";
+
+  let personInfo = document.createElement("div");
+  personInfo.className = "person-info-wrapper";
+  personInfo.appendChild(personClone);
+  personInfo.appendChild(dateTime);
+
   if (input.value !== "") {
+    postBox.className = "postBox";
     textPost.className = "text";
     textPost.textContent = input.value;
-    post.appendChild(textPost);
+
+    postBox.appendChild(personInfo);
+    postBox.appendChild(textPost);
+    post.appendChild(postBox);
+
     input.value = "";
     postContainer.style.display = "none";
   }
 }
 
-
-// on keydown
 document.addEventListener("DOMContentLoaded", function () {
-  const input = document.querySelector(".input-container input");
+  let input = document.querySelector(".input-container input");
 
   input.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
