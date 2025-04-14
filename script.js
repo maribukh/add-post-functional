4fb58fa421e39629e32b9be4e7fc650c4e0a80da
Mariam <mariebukhaidze@gmail.com>
2025-04-14 11:44:58 +0400

    Added New Post

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function togglePostDetails() {
   }
 }
 
+function toggleAddPost() {
+  let;
+}
+
 function deleteButton(element) {
   let postContainer = element.closest(".post-container");
   postContainer.style.display = "none";
@@ -18,4 +22,19 @@ function deleteButton(element) {
   let input = document.querySelector(".input-container input");
 
   input.value = "";
+}
+
+function addPost() {
+  let input = document.querySelector(".input-container input");
+  let post = document.querySelector(".post");
+  let textPost = document.createElement("p");
+  let postContainer = document.querySelector(".post-container");
+
+  if (input.value !== "") {
+    textPost.className = "text";
+    textPost.textContent = input.value;
+    post.appendChild(textPost);
+    input.value = "";
+    postContainer.style.display = "none";
+  }
 }
